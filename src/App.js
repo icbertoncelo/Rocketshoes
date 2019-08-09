7192eed0acb4e167a75e5bc7e7ee452988410a21
Ian Carlos <carlos.ian007@gmail.com>
2019-08-09 16:38:01 -0300

    Style(header): Add structure, logo and routes navigation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <Header />
       <Routes />
     </BrowserRouter>
   );
