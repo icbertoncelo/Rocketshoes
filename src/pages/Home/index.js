4b26ce88a77e2ddfd744f4503c0183a375acec87
Ian Carlos <carlos.ian007@gmail.com>
2019-08-12 09:47:18 -0300

    Style(home): Add product list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList, Product, ShoppButton } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <Product>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-evasion-masculino/26/D18-4157-026/D18-4157-026_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+
+        <strong>Tênis para corrida</strong>
+        <span>R$199,00</span>
+
+        <ShoppButton type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </ShoppButton>
+      </Product>
+    </ProductList>
   );
 }
