abb93bd01b159dcd2dcee28dac9781019a9c4b86
Ian Carlos <carlos.ian007@gmail.com>
2019-08-12 10:48:44 -0300

    Feat(api): Add json-server api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList, Product, ShoppButton } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <Product>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-evasion-masculino/26/D18-4157-026/D18-4157-026_detalhe2.jpg?resize=326:*"
-          alt="Tênis"
-        />
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <strong>Tênis para corrida</strong>
-        <span>R$199,00</span>
+  async componentDidMount() {
+    const response = await api.get('/products');
 
-        <ShoppButton type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </ShoppButton>
-      </Product>
-    </ProductList>
-  );
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <Product>
+            <img src={product.image} alt={product.title} />
+
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <ShoppButton type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </ShoppButton>
+          </Product>
+        ))}
+      </ProductList>
+    );
+  }
 }
